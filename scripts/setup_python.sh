5bc867bf838d997b02f58978e548f0d8153f647f
Paul Brabban <paul.brabban@gmail.com>
2025-07-09 11:11:56 +0000

    fix up use of apt in AI assisted script

--- scripts/setup_python.sh
+++ scripts/setup_python.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
-apt update
+apt-get update
 
 # Install Python, venv and pip - we'll set an env var to prevent installations to system for a better error message
-apt install -y python3 python3-venv
+apt-get install -y python3 python3-venv
 
 # Create a global alias for 'python' to 'python3' and set pip to only run in a venv
 tee /etc/python_setup.sh <<EOF
