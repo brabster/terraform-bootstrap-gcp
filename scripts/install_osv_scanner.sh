ade5912ab54d031c06b6978046fe2f864d430014
Paul Brabban <paul.brabban@gmail.com>
2025-08-08 10:22:24 +0100

    Move osv-scanner to latest now 2.2.0 is released

--- scripts/install_osv_scanner.sh
+++ scripts/install_osv_scanner.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 TMP_DIR=$(mktemp -d)
 cd $TMP_DIR
 
-VERSIONED_PATH=https://github.com/google/osv-scanner/releases/download/v2.0.3/
+RELEASE_URL=https://github.com/google/osv-scanner/releases/latest/download
 
-wget -q ${VERSIONED_PATH}/osv-scanner_linux_amd64
-wget -q ${VERSIONED_PATH}/osv-scanner_SHA256SUMS
+wget -q ${RELEASE_URL}/osv-scanner_linux_amd64
+wget -q ${RELEASE_URL}/osv-scanner_SHA256SUMS
 egrep 'osv-scanner_linux_amd64$' osv-scanner_SHA256SUMS | sha256sum --check
 
 cp osv-scanner_linux_amd64 /usr/local/bin/osv-scanner
